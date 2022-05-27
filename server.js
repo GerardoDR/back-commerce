9f274ab443d11bcc24e8e3fadf87830520c7b2f8
Gerardo Rodriguez <gdrodriguezok@gmail.com>
2022-05-27 00:25:19 -0300

    pre-entrega2 test

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const express = require("express");
+import mongoose from "mongoose";
+import express from "express";
+import productRouter from "./src/routes/ProductRoutes";
+import cartRouter from "./src/routes/CartRoutes";
+import * as model from "./src/db/mongoDB";
+
 const app = express();
-const productRouter = require("./src/routes/ProductRoutes");
-const cartRouter = require("./src/routes/CartRoutes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,8 +13,27 @@ app.use("/api/products", productRouter);
 app.use("/api/carts", cartRouter);
 app.get("*", (req, res) => {
   res.status(404).send("Not found");
-})
+});
 
 app.listen(8080, () => {
   console.log("Server on port https://localhost:8080");
 });
+
+const productSaveModel = new model.products({
+  name: "Product 1",
+  description: "Description 1",
+  timestamp: Date.now(),
+  code: "Product1",
+  thumbnail: "https://via.placeholder.com/150",
+  price: 100,
+  stock: 10,
+});
+
+let productToDB = await productSaveModel
+  .save()
+  .then(() => {
+    console.log("Product saved");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
